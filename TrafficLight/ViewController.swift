3ff99584539beafbefcf05b9d818b8729e90982e
Ilya Osipov <osiios.dev@gmail.com>
2022-07-31 21:45:29 +0300

    1. Added outlets

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var collorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = redView.frame.width
+        yellowView.layer.cornerRadius = yellowView.frame.width
+        greenView.layer.cornerRadius = greenView.frame.width
+        
+        redView.alpha = 0.1
+        yellowView.alpha = 0.1
+        greenView.alpha = 0.1
+        
+        collorButton.layer.cornerRadius = 20
     }
-
-
+    
+    @IBAction func changeAlphaButton() {
+    }
+    
 }
 
