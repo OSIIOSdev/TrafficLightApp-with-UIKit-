330aa16117f3b50473fbbb9cf29870b7b9928329
Ilya Osipov <osiios.dev@gmail.com>
2022-07-31 23:27:32 +0300

    1. bugs fixed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -12,23 +12,25 @@ class ViewController: UIViewController {
     @IBOutlet var redView: UIView!
     @IBOutlet var yellowView: UIView!
     @IBOutlet var greenView: UIView!
-    @IBOutlet var collorButton: UIButton!
+    
+    @IBOutlet var changeButton: UIButton!
+    
+    @IBOutlet var viewsCollection: [UIView]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         redView.layer.cornerRadius = redView.frame.width
         yellowView.layer.cornerRadius = yellowView.frame.width
         greenView.layer.cornerRadius = greenView.frame.width
         
-        redView.alpha = 0.1
-        yellowView.alpha = 0.1
-        greenView.alpha = 0.1
+        redView.alpha = 0.2
+        yellowView.alpha = 0.2
+        greenView.alpha = 0.2
         
-        collorButton.layer.cornerRadius = 20
+        changeButton.layer.cornerRadius = 20
     }
-    
-    @IBOutlet var collorViewsCollection: [UIView]!
-    
+
     @IBAction func changeAlphaButton() {
         
     }
